2e5bfef7f3b949789488dabc0b266897d4c100c5
Araika Singh <araikasingh@gmail.com>
2021-09-27 22:30:50 +0530

    Setup structure & DB with docker

---
+++ src/main/java/com/rayeka/SpringBootBrushUp/SpringBootBrushUpApplication.java
@@ -0,0 +1,13 @@
+package com.rayeka.SpringBootBrushUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBrushUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBrushUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rayeka/SpringBootBrushUp/SpringBootBrushUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rayeka.SpringBootBrushUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBrushUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
